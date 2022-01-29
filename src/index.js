5a1baaf5ea29ab71d0ec5329a33eb31810a9af76
M0rl0ck <seriy17@yandex.ru>
2022-01-29 19:51:29 +0300

    complete task

--- src/index.js
+++ src/index.js
@@ -1,3 +1,33 @@
 module.exports = function check(str, bracketsConfig) {
+  let stack = [];
+  let closeBrakets = {};
+  let openBackets = bracketsConfig.map(item => {
+     closeBrakets[item[1]] = item[0];
+     return item[0]});
+
+     for (let i = 0; i < str.length; i++) {
+       let simbol = str[i];
+       if (openBackets.includes(simbol)) {
+         if (simbol in closeBrakets) { 
+           if (stack.length === 0) {stack.push(simbol)} else {
+               if (stack[stack.length - 1] === simbol) {
+                 stack.pop();
+                } else {stack.push(simbol)};
+           }
+         } else {
+           stack.push(simbol);}
+
+       } else {
+          if (stack.length === 0) {
+             return false;
+          } 
+          let top = stack[stack.length - 1];
+          if (closeBrakets[simbol] === top) {stack.pop()} else {return false};   
+       }
+     }
+
+     return (stack.length === 0);
+   
+
   // your solution
 }
